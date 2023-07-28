4e14c1e5ef6cb231028ffa4823f4992a3b44e132
Sergio Souza <serjsouza93@gmail.com>
2023-07-28 20:04:52 -0300

    switch background image

--- index.js
+++ index.js
@@ -5,8 +5,32 @@ const slideImg4 = document.querySelector(".slideImg4");
 
 const slideButton = document.querySelector(".div-btn");
 
+
+const mountain1 = document.querySelector(".option1")
+const mountain2 = document.querySelector(".option2")
+const sectionBackground = document.querySelector(".section3")
+
+function changeBackgroundImageMountain1() {
+  sectionBackground.style.backgroundImage = "url(../images/background3.png)"  
+  mountain2.style.backgroundColor = "#48546B"
+  mountain1.style.backgroundColor = "rgba(104,121,153, 0.5)"
+}
+
+function changeBackgroundImageMountain2() {
+  sectionBackground.style.backgroundImage = "url(../images/background4.png)"
+  mountain1.style.backgroundColor = "#48546B"
+  mountain2.style.backgroundColor = "rgba(104,121,153, 0.5)"  
+}
+
+
 function scrollPage() {
   document.querySelector(".section2").scrollIntoView({
+    behavior: "smooth",
+  });
+}
+
+function scrollPage2() {
+  document.querySelector(".section3").scrollIntoView({
     behavior: "smooth",
   });
 }
